4bcb8e800381c3e55cd4c4a540b0961d168b3e4d
Naimbouz <damdoumbouz@gmail.com>
2024-12-05 22:31:42 +0100

    init

---
+++ src/main/java/com/example/locationdevoiture/User.java
@@ -0,0 +1,32 @@
+package com.example.locationdevoiture;
+
+public class User {
+    private String username;
+    private Role role;
+
+    public enum Role {
+        ADMIN,
+        CLIENT
+    }
+
+    public User(String username, Role role) {
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
